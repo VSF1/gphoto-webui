12a865dd1de6111f31e93674b93035283abbecc5
VSF1 <vitorricardofonseca@gmail.com>
2024-04-04 18:12:46 +0100

    updated

--- bin/tetherHook.sh
+++ bin/tetherHook.sh
@@ -4,22 +4,22 @@ FILENAME=${ARGUMENT}
 if [[ $FILENAME =~ .+\.[jpg|JPG] ]];then
   #if [ *$ACTION* = *Saving* ]; then
   # do a echo of the file name
-  echo "Download: $FILENAME"
+#  echo "Download: $FILENAME"
   # shot the picture in the current geeqie open
   # cp $ARGUMENT latest.jpg
-#  /usr/bin/geeqie --remote "$ARGUMENT" &
-  mv "$ARGUMENT" "$FILENAME"
-  md5sum "$FILENAME" > "$FILENAME.md5"
+  #/usr/bin/geeqie --remote "$ARGUMENT" &
+  #mv "$ARGUMENT" "$FILENAME"
+  #md5sum "$FILENAME" > "$FILENAME.md5"
 fi
 
 if [[ $FILENAME =~ .+\.[arw|ARW|mrw|MRW] ]];then
   #if [ *$ACTION* = *Saving* ]; then
   # do a echo of the file name
-  echo "Download: $FILENAME"
+#  echo "Download: $FILENAME"
   # shot the picture in the current geeqie open
   # cp $ARGUMENT latest.arw
 #  /usr/bin/geeqie --remote "$ARGUMENT" &
-  mv "$ARGUMENT" "$FILENAME"
-  md5sum "$FILENAME" > "$FILENAME.md5"
+  #mv "$ARGUMENT" "$FILENAME"
+  #md5sum "$FILENAME" > "$FILENAME.md5"
 fi
  
